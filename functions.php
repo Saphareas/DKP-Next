ea6e7f70814f5989fdfcb9ba3b18e5ff15eb901c
Fabian Große <saphareas@googlemail.com>
2018-11-17 12:58:59 +0100

    Require/Recommend some plugins

--- functions.php
+++ functions.php
@@ -13,3 +13,48 @@ endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
 // END ENQUEUE PARENT ACTION
+
+require_once get_stylesheet_directory() . '/class-tgm-plugin-activation.php';
+add_action( 'tgmpa_register', 'DKP_Next_register_required_plugins' );
+
+function DKP_Next_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+		array(
+			'name'      => 'Github Updater',
+			'slug'      => 'github-updater',
+            'source'    => 'https://github.com/afragen/github-updater/archive/master.zip',
+            'required'  => false,
+        ),
+        array(
+			'name'      => 'amr shortcode any widget',
+			'slug'      => 'amr-shortcode-any-widget',
+			'required'  => false,
+		),
+	);
+
+	/*
+	 * Array of configuration settings. Amend each line as needed.
+	 *
+	 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+	 * strings available, please help us make TGMPA even better by giving us access to these translations or by
+	 * sending in a pull-request with .po file(s) with the translations.
+	 */
+	$config = array(
+		'id'           => 'DKP-Next',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
